49a4499c27ad2655bcb3a23c0035d642f0f25075
Henrique <henriquecerquinho@gmail.com>
2019-05-28 17:09:12 -0300

    edit e delete funcionando, aparentemente

--- agenda/app/controllers/atividades_controller.rb
+++ agenda/app/controllers/atividades_controller.rb
@@ -8,11 +8,40 @@ class AtividadesController < ApplicationController
         redirect_to @atividade
     end
 
+    def new
+        @atividade = Atividade.new
+    end
+
     def show
         @atividade = Atividade.find(params[:id])
     end
 
     def index
         @atividades = Atividade.all
+    end
+
+    def destroy
+        @atividade = Atividade.find(params[:id])
+        @atividade.destroy
+
+        redirect_to atividades_path
+    end
+
+    def edit
+        @atividade = Atividade.find(params[:id])
+    end
+
+    def update
+        @atividade = Atividade.find(params[:id])
+        if @atividade.update(atividades_params)
+            redirect_to @atividade
+        else
+            render 'edit'
+        end
+    end
+
+    private
+    def atividades_params
+       params.require(:atividade).permit(:nome, :horario_inicio, :horario_fim, :sala, :professor)
     end
 end
--- agenda/config/routes.rb
+++ agenda/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :atividades
   root 'manda#index'
   get 'index' => 'avisos#index'
+  post 'atividades/new', to: 'atividades#new'
   resources :avisos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
