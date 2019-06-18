08200f9e0a95188f6bed360bbc53e2c6beb31c57
Henrique <henriquecerquinho@gmail.com>
2019-06-18 20:59:38 -0300

    validação para conflitos entre horarios de atividades, assim como professor presente em duas atividades ao mesmo tempo

--- agenda/app/models/atividade.rb
+++ agenda/app/models/atividade.rb
@@ -1,3 +1,26 @@
 class Atividade < ApplicationRecord
     validates_presence_of :nome, :sala, :professor, :horario_inicio, :horario_fim
+    validate :conflitos
+
+    private
+
+    def conflitos
+        atividades = Atividade.all
+
+        atividades.each do |atv|
+            if atv.nome != nome # que nao seja a atividade que estamos inserindo
+                # checa se tem mais de uma atividade ao mesmo tempo na sala
+                if atv.sala == sala
+                    if atv.horario_fim > horario_inicio and atv.horario_inicio < horario_fim
+                        errors.add(atv.nome,  "ja acontecerá nessa sala e nesse horario!")
+                    end
+                # checa se o professor esta em duas aulas ao mesmo tempo
+                elsif atv.professor == professor
+                    if atv.horario_fim > horario_inicio and atv.horario_inicio < horario_fim
+                        errors.add(atv.horario_inicio.to_s, atv.professor + " está ocupado nesse horario!")
+                    end
+                end
+            end
+        end
+    end
 end
