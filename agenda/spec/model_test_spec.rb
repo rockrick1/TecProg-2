f556d19820fb5a70e43a9ebb246d7c60550e50af
Henrique <henriquecerquinho@gmail.com>
2019-06-25 14:04:32 -0300

    fix nos erros aparecendo todos na mesma linha
    testes de conflitos funcionando e completos

--- agenda/spec/model_test_spec.rb
+++ agenda/spec/model_test_spec.rb
@@ -32,17 +32,41 @@ RSpec.describe Atividade, type: :model do
                horario_fim: "13:30", sala: "b10", professor: "Eminem")).to_not be_valid
     end
 
-############################## Testes de conflitos #############################
-        # let!(:atividade1) { Atividade.new(nome: "Tecprog", horario_inicio: "10:00",
-                            # horario_fim: "11:00", sala: "b10", professor: "Jonas Brothers") }
-        # let!(:atividade2) { Atividade.new(nome: "Analise", horario_inicio: "10:30",
-                            # horario_fim: "11:30", sala: "b10", professor: "Coelho") }
-#         it "| two activities cant happen in the same class at the same time" do
-#             expect { delete :new, params: { nome: "Tecprog", horario_inicio: "10:00",
-#                                             horario_fim: "11:00", sala: "b10", professor: "Jonas Brothers" }
-#                      delete :new, params: { nome: "Analise", horario_inicio: "10:30",
-#                                             horario_fim: "11:30", sala: "b10", professor: "Coelho"}
-#             }.to change { Atividade.all.count }.by(2)
-#             # .to raise_error
-#         end
+
+
+    ############################## Testes de conflitos #############################
+
+    it "| two activities cant happen in the same class at the same time" do
+        ativ1 = Atividade.new(nome: "Tecprog", horario_inicio: "10:00",
+               horario_fim: "11:00", sala: "b10", professor: "Jonas Brothers")
+        ativ1.save
+        #conflito começando depois
+        expect(Atividade.new(nome: "Analise", horario_inicio: "10:30",
+                      horario_fim: "11:30", sala: "b10", professor: "Coelho")).to_not be_valid
+        #conflito começando antes
+        expect(Atividade.new(nome: "Analise", horario_inicio: "9:30",
+                      horario_fim: "10:30", sala: "b10", professor: "Coelho")).to_not be_valid
+        #conflito no mesmo horário
+        expect(Atividade.new(nome: "Analise", horario_inicio: "10:00",
+                      horario_fim: "11:00", sala: "b10", professor: "Coelho")).to_not be_valid
+        #começando antes e acabando depois
+        expect(Atividade.new(nome: "Analise", horario_inicio: "9:00",
+            horario_fim: "13:00", sala: "b10", professor: "Coelho")).to_not be_valid
+    end
+
+    it "| same professor cant be in different classes at the same time" do
+        ativ1 = Atividade.new(nome: "Tecprog", horario_inicio: "10:00",
+               horario_fim: "11:00", sala: "b10", professor: "Gubi")
+        ativ1.save
+
+        expect(Atividade.new(nome: "Analise", horario_inicio: "10:00",
+                      horario_fim: "11:00", sala: "b11", professor: "Gubi")).to_not be_valid
+    end
+
+    ############################## Teste de validade do horário #############################
+
+        it "needs to have valid duration" do
+            expect(Atividade.new(nome: "Tecprog2", horario_inicio: "11:00",
+                          horario_fim: "10:00", sala: "b10", professor: "Gubi")).to_not be_valid
+        end
 end
