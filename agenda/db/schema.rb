fe14d94b64c9a26d92359a875ab864409b4d1ac4
Gabriel Araújo <gabrielaraujo@Gabriels-MacBook-Air.local>
2019-06-26 18:37:30 -0300

    login

--- agenda/db/schema.rb
+++ agenda/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_172959) do
+ActiveRecord::Schema.define(version: 2019_06_26_012017) do
 
   create_table "atividades", force: :cascade do |t|
     t.string "nome"
@@ -27,6 +27,17 @@ ActiveRecord::Schema.define(version: 2019_05_28_172959) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", limit: 128, null: false
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128, null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
 end
